358f7acff61d0fb72b5f82685e6f5229a0c5ba2a
igwechinonso <igwechinonso77@gmail.com>
2022-11-08 12:39:47 +0100

    refactor logger file

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
-import { logger } from '../utils/logger';
+// import { logger } from '../utils/logger';
 
 export const db = {
   connect: () => {
     mongoose.connect(`${process.env.DB_USER}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
     const db = mongoose.connection;
-    db.on("error", () => logger('error', 'Database Connection: Error while connecting'));
+    db.on("error", () => console.log('error', 'Database Connection: Error while connecting'));
     db.once("open", function () {
-      logger('info', 'Database Connection: Connection was successfull');
+      console.log('info', 'Database Connection: Connection was successfull');
     });
   }
 };
