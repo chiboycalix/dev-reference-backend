2170c5ab0c511c2b6d3c66b3e4d1b1b9f088b7eb
igwechinonso <igwechinonso77@gmail.com>
2022-11-08 17:35:33 +0100

    create post

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import authRoutes from '../modules/auth/auth.routes';
+import postRouter from '../modules/post/post.routes';
 
 const router = express.Router();
 
 const use = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 
 router.use('/api/v1', use(authRoutes));
+router.use('/api/v1', use(postRouter));
 
 export default router;
 
